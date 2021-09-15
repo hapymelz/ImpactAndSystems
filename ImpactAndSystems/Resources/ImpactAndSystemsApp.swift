ad56ddd7c4cd47fb54b9b7a7f7ef1428da1a9533
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-09-15 08:34:31 -0600

    created new repo.

---
+++ ImpactAndSystems/Resources/ImpactAndSystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpactAndSystemsApp.swift
+//  ImpactAndSystems
+//
+//  Created by Numbers, Katherine on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ImpactAndSystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
